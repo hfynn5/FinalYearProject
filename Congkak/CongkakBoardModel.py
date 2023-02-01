a33c702a0b0a7cfab22ebb564db1f021e34f3dc1
aiminer357 <64268501+Aiminer357@users.noreply.github.com>
2023-02-01 17:16:07 +0800

    added hand class and moved dropping counters into another method

--- Congkak/CongkakBoardModel.py
+++ Congkak/CongkakBoardModel.py
@@ -3,14 +3,35 @@ from Congkak.CongkakBoardGraphics import BoardGraphic
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import QPixmap
 
+class Hand:
+    player = 'n'
+    hole_pos = 0;
+    counter_count = 0;
+
+    def __init__(self, player, hole_pos, counter_count):
+        self.player = player
+        self.hole_pos = hole_pos
+        self.counter_count = counter_count
+
+
 class BoardModel:
+
+    # hand_a_dict = {
+    #     'hole': 0,
+    #     'counter_count': 0
+    # }
+    #
+    # hand_b_dict = {
+    #     'hole': 0,
+    #     'counter_count': 0
+    # }
 
     # player A is top with storeroom on right.
     # player B is bottom with storeroom on left.
     storeroom_a_value = 0
     storeroom_b_value = 0
 
-    # player A house 1 starts at right. For the sake of sanity, houses start at 1, not 0
+    # player A house 1 starts at left. For the sake of sanity, houses start at 1, not 0
     # player B house 1 starts at left.
     house_a_values = [0, 0, 0, 0, 0, 0, 0]
     house_b_values = [0, 0, 0, 0, 0, 0, 0]
@@ -21,8 +42,8 @@ class BoardModel:
         App = QApplication(sys.argv)
         self.board_graphic = BoardGraphic()
 
-        self.house_a_values = [7, 7, 7, 7, 7, 7, 7]
-        self.house_b_values = [7, 7, 7, 7, 7, 7, 7]
+        self.house_a_values = [1, 7, 7, 7, 7, 7, 7]
+        self.house_b_values = [1, 0, 0, 0, 0, 0, 0]
 
         self.storeroom_a_value = 0
         self.storeroom_b_value = 0
@@ -31,10 +52,9 @@ class BoardModel:
 
         # time.sleep(1)
 
-        self.iterate_sowing(player='a', hole=1)
+        self.iterate_sowing(player='a', hole=6)
 
         self.update_board()
-
 
         sys.exit(App.exec())
 
@@ -64,10 +84,6 @@ class BoardModel:
                 hole = new_hand_pos % 10
                 print("continuing starting with: " + str(hole))
 
-
-
-
-
     def sow_once(self, player, hole):
 
         hand_value = 0
@@ -87,31 +103,42 @@ class BoardModel:
             hand_value = self.house_b_values[hole-1]
             self.house_b_values[hole-1] = 0
 
-        while hand_value > 0:
+        player_hand = Hand(player=player, hole_pos=hand_pos, counter_count=hand_value)
+
+        while player_hand.counter_count > 0:
 
             # time.sleep(1)
             self.update_board()
 
-            hand_pos -= 1
+            player_hand.hole_pos -= 1
 
-            if hand_pos == 10:
-                if player == 'a':
-                    self.storeroom_a_value += 1
-                    hand_value -= 1
-                hand_pos = 28
-            elif hand_pos == 20:
-                if player == 'b':
-                    self.storeroom_b_value += 1
-                    hand_value -= 1
-                hand_pos = 18
-            elif hand_pos < 20:
-                self.house_a_values[hand_pos-11] += 1
-                hand_value -= 1
-            elif hand_pos > 20:
-                self.house_b_values[hand_pos-21] += 1
-                hand_value -= 1
+            # hand_pos -= 1
 
-        return hand_pos
+            player_hand = self.drop_counter(player_hand)
+
+        return player_hand.hole_pos
+
+    def drop_counter(self, hand):
+        if hand.hole_pos == 10:
+            if hand.player == 'a':
+                self.storeroom_a_value += 1
+                hand.counter_count -= 1
+            hand.hole_pos = 28
+        elif hand.hole_pos == 20:
+            if hand.player == 'b':
+                self.storeroom_b_value += 1
+                hand.counter_count -= 1
+            hand.hole_pos = 18
+        elif hand.hole_pos < 20:
+            self.house_a_values[hand.hole_pos - 11] += 1
+            hand.counter_count -= 1
+        elif hand.hole_pos > 20:
+            self.house_b_values[hand.hole_pos - 21] += 1
+            hand.counter_count -= 1
+
+        return hand
+
+
 
     def update_board(self):
         self.board_graphic.update_values(house_a_values=self.house_a_values, house_b_values=self.house_b_values,
