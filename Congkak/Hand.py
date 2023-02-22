90e99c5d212db585ea7ed58d406cefd5dba16775
aiminer357 <64268501+Aiminer357@users.noreply.github.com>
2023-02-22 17:21:52 +0800

    Added function to hand

--- Congkak/Hand.py
+++ Congkak/Hand.py
@@ -7,3 +7,12 @@ class Hand:
         self.player = player
         self.hole_pos = hole_pos
         self.counter_count = counter_count
+
+    def drop_one_counter(self):
+        self.counter_count -= 1
+        return 1
+
+    def drop_all_counters(self):
+        old = self.counter_count
+        self.counter_count = 0
+        return old
