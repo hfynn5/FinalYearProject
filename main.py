5b5cc439f0e69fdf9e23d3d13d89b59af7da1f5d
aiminer357 <64268501+Aiminer357@users.noreply.github.com>
2023-03-07 14:24:52 +0800

    potential fix to overflow error

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from graphics import *
-import tkinter
 import Congkak.GameManager as CongkakGame
+import sys
 
-
+sys.setrecursionlimit(4000)
 congkak = CongkakGame.GameManager()
+
